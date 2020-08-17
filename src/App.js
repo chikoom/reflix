fe3420b94476b7b3847ff2e53f984fab8509527d
Chikoom <callforcopy@gmail.com>
2020-08-17 18:56:46 +0300

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Landing from './components/Landing'
 import Catalog from './components/Catalog'
@@ -7,54 +7,56 @@ import Nav from './components/Nav'
 import moviesData from './data/movies'
 import './App.css'
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      users: [
-        {
-          id: 0,
-          name: 'default',
-          color: 'gray',
-          rentedMovies: [],
-          budget: 10,
-        },
-        ...this.getUsersFromStorage(),
-      ],
+const App = () => {
+  const [users, setUsers] = useState([
+    {
+      id: 0,
+      name: 'default',
+      color: 'gray',
+      rentedMovies: [],
       budget: 10,
-      movies: moviesData,
-    } // endof State
+    },
+  ])
+  const [loggedUser, setLoggedUser] = useState('default')
+  const [budget, setBudget] = useState(10)
+  const [movies, setMovies] = useState(moviesData)
+
+  const getUsersFromStorage = () => {
+    if (!localStorage.getItem('reflixStorage')) {
+      localStorage.setItem(
+        'reflixStorage',
+        JSON.stringify({ loggedUser: loggedUser, users: [] })
+      )
+    }
+    const reflixStorage = JSON.parse(localStorage.getItem('reflixStorage'))
+    return reflixStorage.users
   }
-  deleteUser = userId => {
+
+  const deleteUser = userId => {
     userId = parseInt(userId)
-    console.log('userId', userId)
-    const currentUsers = [...this.state.users]
-    console.log('currentUsers', currentUsers)
+    const currentUsers = [...users]
     const filteredUsers = currentUsers.filter(
       user => parseInt(user.id) !== parseInt(userId)
     )
-    console.log('filteredUsers', filteredUsers)
-    this.setState(
-      {
-        users: filteredUsers,
-      },
-      () => {
-        this.removeUserFromStorage(userId)
-      }
-    )
+    setUsers(filteredUsers)
+    removeUserFromStorage(userId)
   }
-  removeUserFromStorage = userId => {
+  useEffect(() => {
+    const storageUsers = getUsersFromStorage()
+    setUsers([...users, ...storageUsers])
+  }, [])
+  const removeUserFromStorage = userId => {
     localStorage.setItem(
       'reflixStorage',
       JSON.stringify({
-        loggedUser: this.state.loggedUser,
-        users: this.state.users.filter(user => user.id !== 0),
+        loggedUser: loggedUser,
+        users: users.filter(user => user.id !== 0),
       })
     )
   }
-  saveNewUser = (username, color) => {
+  const saveNewUser = (username, color) => {
     let maxId = 0
-    this.state.users.forEach(user => {
+    users.forEach(user => {
       if (user.id > maxId) maxId = user.id + 1
     })
     ++maxId
@@ -66,32 +68,16 @@ class App extends Component {
       rentedMovies: [],
       budget: 10,
     }
-    const currentUsers = [...this.state.users]
+    const currentUsers = [...users]
     currentUsers.push(newUser)
-    this.setState(
-      {
-        users: currentUsers,
-      },
-      () => {
-        this.insertUserToStorage(newUser.id)
-      }
-    )
+    setUsers(currentUsers)
+    insertUserToStorage(newUser.id)
   }
-  getUsersFromStorage = () => {
-    if (!localStorage.getItem('reflixStorage')) {
-      localStorage.setItem(
-        'reflixStorage',
-        JSON.stringify({ loggedUser: this.state.loggedUser, users: [] })
-      )
-    }
-    const reflixStorage = JSON.parse(localStorage.getItem('reflixStorage'))
 
-    return reflixStorage.users
-  }
-  insertUserToStorage = userId => {
+  const insertUserToStorage = userId => {
     if (userId !== 0) {
-      const userDetails = this.state.users.find(user => user.id === userId)
-      const storageUsers = this.getUsersFromStorage()
+      const userDetails = users.find(user => user.id === userId)
+      const storageUsers = getUsersFromStorage()
       const usersWithout = storageUsers.filter(
         user => user.id !== userDetails.id
       )
@@ -99,120 +85,103 @@ class App extends Component {
       localStorage.setItem(
         'reflixStorage',
         JSON.stringify({
-          loggedUser: this.state.loggedUser,
+          loggedUser: loggedUser,
           users: usersWithout,
         })
       )
     }
   }
-  logUserIn = userId => {
+  const logUserIn = userId => {
     if (!localStorage.getItem('reflixStorage')) {
       localStorage.setItem(
         'reflixStorage',
-        JSON.stringify({ loggedUser: this.state.loggedUser, users: [] })
+        JSON.stringify({ loggedUser: loggedUser, users: [] })
       )
     }
     const reflixStorage = JSON.parse(localStorage.getItem('reflixStorage'))
-    this.setState(
-      {
-        loggedUser: userId,
-      },
-      () => {
-        reflixStorage.loggedUser = userId
-        localStorage.setItem('reflixStorage', JSON.stringify(reflixStorage))
-      }
-    )
+    setLoggedUser(userId)
+    reflixStorage.loggedUser = userId
+    localStorage.setItem('reflixStorage', JSON.stringify(reflixStorage))
   }
-  newRentReturnMovie = (userId, movieId, isRented) => {
-    if (this.newUpdateBudget(userId, isRented)) {
-      const users = [...this.state.users]
-      const userMovies = users.find(user => user.id === userId).rentedMovies
+  const newRentReturnMovie = (userId, movieId, isRented) => {
+    if (newUpdateBudget(userId, isRented)) {
+      const newUsers = [...users]
+      const userMovies = newUsers.find(user => user.id === userId).rentedMovies
       if (isRented) {
         userMovies.push(movieId)
       } else {
         let movieIndex = userMovies.findIndex(id => id === movieId)
         userMovies.splice(movieIndex, 1)
       }
-      this.setState(
-        {
-          users,
-        },
-        () => {
-          this.insertUserToStorage(userId)
-        }
-      )
+
+      setUsers(newUsers)
+      insertUserToStorage(userId)
     } else {
       alert('You dont have the budget!')
       return false
     }
   }
-  newUpdateBudget(userId, isRented) {
-    const users = [...this.state.users]
+  const newUpdateBudget = (userId, isRented) => {
+    const newUsers = [...users]
     let budgetChange = isRented ? -3 : 3
-    const currentUser = users.find(user => user.id === userId)
+    const currentUser = newUsers.find(user => user.id === userId)
     if (currentUser.budget + budgetChange > 0) {
       currentUser.budget += budgetChange
-      this.setState({
-        users: users,
-      })
+      setUsers(newUsers)
       return true
     }
     return false
   }
-  render() {
-    return (
-      <Router>
-        <div className='App'>
-          <Nav />
-          <Route
-            exact
-            path='/'
-            render={() => (
-              <Landing
-                deleteUser={this.deleteUser}
-                saveNewUser={this.saveNewUser}
-                logUserIn={this.logUserIn}
-                users={this.state.users}
-              />
-            )}
-          />
-          <Route
-            exact
-            path='/catalog'
-            render={({ match }) => (
-              <Catalog
-                users={this.state.users}
-                match={match}
-                rentReturnMovie={this.rentReturnMovie}
-                movies={this.state.movies}
-                budget={this.state.budget}
-              />
-            )}
-          />
-          <Route
-            exact
-            path='/catalog/u/:id'
-            render={({ match }) => (
-              <Catalog
-                users={this.state.users}
-                match={match}
-                rentReturnMovie={this.newRentReturnMovie}
-                movies={this.state.movies}
-                budget={this.state.budget}
-              />
-            )}
-          />
-          <Route
-            exact
-            path='/movies/:id'
-            render={({ match }) => (
-              <MovieDetails match={match} movies={this.state.movies} />
-            )}
-          />
-        </div>
-      </Router>
-    )
-  }
+  return (
+    <Router>
+      <div className='App'>
+        <Nav />
+        <Route
+          exact
+          path='/'
+          render={() => (
+            <Landing
+              deleteUser={deleteUser}
+              saveNewUser={saveNewUser}
+              logUserIn={logUserIn}
+              users={users}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/catalog'
+          render={({ match }) => (
+            <Catalog
+              users={users}
+              match={match}
+              rentReturnMovie={newRentReturnMovie}
+              movies={movies}
+              budget={budget}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/catalog/u/:id'
+          render={({ match }) => (
+            <Catalog
+              users={users}
+              match={match}
+              rentReturnMovie={newRentReturnMovie}
+              movies={movies}
+              budget={budget}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/movies/:id'
+          render={({ match }) => <MovieDetails match={match} movies={movies} />}
+        />
+      </div>
+    </Router>
+  )
 }
 
 export default App
